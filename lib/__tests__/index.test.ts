0e733f5edb1ceb3c669d152da243f3d982eed98f
Mayank <mayank.srmu@gmail.com>
2025-04-05 13:04:33 +0530

    update tests

--- lib/__tests__/index.test.ts
+++ lib/__tests__/index.test.ts
@@ -14,14 +14,13 @@ const markdown = fs.readFileSync("../sample.md", "utf-8");
 describe.concurrent("toDocx", () => {
   const mdast = unified().use(remarkParse).use(remarkGfm).parse(markdown);
 
-  it("should handle emoji", async ({ expect }) => {
-    const docxBlob = await toDocx(mdast, {}, { plugins: [emojiPlugin()] });
-    expect(docxBlob).toBeInstanceOf(Blob);
-  });
-
-  it("should not have any console.log", async ({ expect }) => {
+  /**
+   * Intentionally combining two tests in one as docx generation could be resource consuming, especially for very large files.
+   */
+  it("should handle emoji and should not have any console.log", async ({ expect }) => {
     const consoleSpy = vi.spyOn(console, "log");
-    await toDocx(mdast, {}, { plugins: [emojiPlugin()] });
+    const docxBlob = await toDocx(mdast, {}, { plugins: [emojiPlugin()] });
     expect(consoleSpy).not.toHaveBeenCalled();
+    expect(docxBlob).toBeInstanceOf(Blob);
   });
 });
